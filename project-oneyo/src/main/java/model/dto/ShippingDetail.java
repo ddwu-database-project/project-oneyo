9c851bc45b2169f6cea0853fbca2191dd65ba823
gynhyunkim <gyeonghyun.kimm@gmail.com>
2021-11-09 15:45:10 +0900

    add: dto/ folder

---
+++ project-oneyo/src/main/java/model/dto/ShippingDetail.java
@@ -0,0 +1,30 @@
+package model;
+
+public class ShippingDetail {
+	private String dateShipped;
+	private String company;
+	private int trackingNum;
+	
+	public String getDateShipped() {
+		return dateShipped;
+	}
+	public void setDateShipped(String dateShipped) {
+		this.dateShipped = dateShipped;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public int getTrackingNum() {
+		return trackingNum;
+	}
+	public void setTrackingNum(int trackingNum) {
+		this.trackingNum = trackingNum;
+	}
+	
+	
+	
+	
+}
