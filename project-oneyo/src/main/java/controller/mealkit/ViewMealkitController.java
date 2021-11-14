329a65d946c82864be16da08aa0bc5963d6b590f
gynhyunkim <gyeonghyun.kimm@gmail.com>
2021-11-14 13:28:25 +0900

    update: merge master branch

--- project-oneyo/src/main/java/controller/mealkit/ViewMealkitController.java
+++ project-oneyo/src/main/java/controller/mealkit/ViewMealkitController.java
@@ -4,32 +4,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import controller.Controller;
 import model.dto.Mealkit;
-<<<<<<< HEAD
-import model.dto.Ingredient;
-import controller.manager.MealkitManager;
-=======
 import model.dao.MealkitDAO;
 import model.dto.Ingredient;
->>>>>>> 65aff69b59e841201f9c8e9767abca13bdbb6e0a
+
 import java.util.List;
 
 public class ViewMealkitController implements Controller{
 	@Override
     public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
 		
-<<<<<<< HEAD
-    	MealkitManager manager = MealkitManager.getInstance();
-    	int mkId = Integer.parseInt(request.getParameter("mkId"));
-    	
-		Mealkit mealkit = manager.findMealkit(mkId);
-		List<Ingredient> mealkitIng = manager.findMealkitIng(mkId);
-=======
 		MealkitDAO mealkitDAO = new MealkitDAO();
 		int mkId = Integer.parseInt(request.getParameter("mkId"));
     	
 		Mealkit mealkit =mealkitDAO.findMealkit(mkId);
 		List<Ingredient> mealkitIng = mealkitDAO.findMealkitIng(mkId);
->>>>>>> 65aff69b59e841201f9c8e9767abca13bdbb6e0a
+
 		mealkit.setIngredients(mealkitIng);
 		
 		request.setAttribute("mealkit", mealkit);	
