95b7d026079d54399cd736cd83471e93db053857
Hyo Gyeong <cdkrcd8@gmail.com>
2021-11-14 13:09:09 +0900

    delete mealkit manager

--- project-oneyo/src/main/java/controller/mealkit/MealkitListController.java
+++ project-oneyo/src/main/java/controller/mealkit/MealkitListController.java
@@ -4,15 +4,15 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import controller.Controller;
+import model.dao.MealkitDAO;
 import model.dto.Mealkit;
-import controller.manager.*;
 
 public class MealkitListController implements Controller {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
 		
-    	MealkitManager manager = MealkitManager.getInstance();
-		List<Mealkit> mealkits = manager.findMealkitList();
+    	MealkitDAO mealkitDAO = new MealkitDAO();
+		List<Mealkit> mealkits = mealkitDAO.findMealkitList();
 		
 		request.setAttribute("mealkits", mealkits);				
 		return "/mealkit/list.jsp";        
