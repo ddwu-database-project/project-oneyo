83d354121ec13b2934a9ac8b79426cef68eb8df2
HyeJun <solar3070@naver.com>
2021-11-18 11:33:44 +0900

    주석 제거

--- project-oneyo/src/main/java/model/dao/OrderDAO.java
+++ project-oneyo/src/main/java/model/dao/OrderDAO.java
@@ -17,7 +17,7 @@ public class OrderDAO {
 	public OrderDAO() {
 		jdbcUtil = new JDBCUtil();
 	}
-	//Ŀ���ҵ� List<Ingredient>��������
+	
 	public List<Ingredient> getCustomIngList(CustomMealkit m){
 		List<Ingredient> customIng = m.getIngredients();
 		String sql = "SELECT i.ingname, i.price, i.calorie, c.ingquantity, "
@@ -39,11 +39,11 @@ public class OrderDAO {
 			ex.printStackTrace();
 		}finally {
 			jdbcUtil.commit();
-			jdbcUtil.close();	// resource ��ȯ
+			jdbcUtil.close();	
 		}
 		return customIng;
 	}
-	//cartController�� �ֹ������� ä���
+
 	public List<CustomMealkit> cartOrderList(List<Integer> id, int customerId){
 		List<CustomMealkit> orderedItems = new ArrayList<>();
 		String sql = "SELECT m.mkname, m.mkId, m.defaultCal, m.defaultPrice, c.price, c.quantity from custommealkit c, mealkit m WHERE c.mkId = m.mkId AND c.customerId = ? AND c.custommkId = ?";
@@ -65,13 +65,11 @@ public class OrderDAO {
 			}
 		}
 		jdbcUtil.commit();
-		jdbcUtil.close();	// resource ��ȯ
+		jdbcUtil.close();	
 		
 		return orderedItems;
 	}
 	
-	//�ֹ� �� ��ȸ
-	//�ֹ� ���� ������ �����ֱ� //�ش� ���� ���±��� �ֹ��� ��� ��ŰƮ����Ʈ �����ִ� ��//CustomMealkit ���̺��� orderStatus=�ֹ���(1)�� �� ��� �����ͼ� ��������
 	public List<CustomMealkit> findOrderList(int customerId) {
 	    List<CustomMealkit> orderedItems = new ArrayList<>();
 	    String sql = "SELECT m.mkname, m.mkId, m.defaultCal, m.defaultPrice, c.price, c.quantity, c.custommkId from custommealkit c, mealkit m WHERE c.mkId = m.mkId AND c.customerId = ?";
@@ -91,12 +89,12 @@ public class OrderDAO {
 			ex.printStackTrace();
 		}finally {
 			jdbcUtil.commit();
-			jdbcUtil.close();	// resource ��ȯ
+			jdbcUtil.close();	
 		}
 		
 		return orderedItems;
 	}
-	//�ֹ�����Ʈ �����ֱ�
+	
 	public List<Order> viewOrderList(int customerId){
 		List<CustomMealkit> orderedItems = findOrderList(customerId);
 		String sql = "SELECT s.orderId, s.orderdate, s.company, s.trackingnum FROM shippingdetail s, orderinfo o WHERE s.orderId=o.orderId AND o.customMkId = ?";
@@ -124,40 +122,36 @@ public class OrderDAO {
 			}
 		}
 		jdbcUtil.commit();
-		jdbcUtil.close();	// resource ��ȯ
+		jdbcUtil.close();	
 		
 		return orderList;
 	}
 	
-	//�ֹ��ϱ�
-	//����Ʈ �޾ƿ� ��(Ŀ���ҹ�ŰƮ��)�� �ϳ��� �ɰ��� �μ�Ʈ
-	
-	//�Ű������� l���� orderId, ingredient �������� �� �Ұ���//generatedKeys()����ؼ� orderId��������
 	public List<CustomMealkit> addOrder(List<CustomMealkit> l){
-		List<CustomMealkit> orderingItems = null; // �ش� ���� ��� customMealkit
+		List<CustomMealkit> orderingItems = null; 
 		Order order = new Order();
 		int totalPrice = order.calcTotalPrice(l);
 		int customerId = l.get(0).getCustomerId();
 		int orderId = 0;
 		
 		System.out.println(customerId);
-		String sql1 = "INSERT INTO MEALKITORDER VALUES(order_seq.NEXTVAL, 0, to_date(sysdate), ?, ?)"; // MEALKITORDER��
-																										// �߰�
+		String sql1 = "INSERT INTO MEALKITORDER VALUES(order_seq.NEXTVAL, 0, to_date(sysdate), ?, ?)"; 
+																										
 
 		Object[] param = new Object[] { customerId, totalPrice };
 		jdbcUtil.setSqlAndParameters(sql1, param);
 
-		String key[] = { "orderId" }; // MEALKITORDER�� PK�÷���
+		String key[] = { "orderId" }; 
 		try {
-			jdbcUtil.executeUpdate(key); // insert�� ����
+			jdbcUtil.executeUpdate(key); 
 			ResultSet rs = jdbcUtil.getGeneratedKeys();
 			if (rs.next()) {
-				orderId = rs.getInt(1); // ������ PK ��
+				orderId = rs.getInt(1); 
 			}
 
 			orderingItems = findOrderList(customerId);
 
-			String sql2 = "INSERT INTO ORDERINFO VALUES(?, ?, order_mk_seq.NEXTVAL, ?)";// ORDERINFO�� �߰�
+			String sql2 = "INSERT INTO ORDERINFO VALUES(?, ?, order_mk_seq.NEXTVAL, ?)";
 
 			for (CustomMealkit item : l) {
 				int customMkId = item.getCustomMealkitId();
@@ -165,13 +159,13 @@ public class OrderDAO {
 				param = new Object[] { customMkId, orderId, quantity };
 				jdbcUtil.setSqlAndParameters(sql2, param);
 
-				int result = jdbcUtil.executeUpdate(); // ó���� ���ڵ� ����
+				int result = jdbcUtil.executeUpdate(); 
 				if (result != 1) {
 					jdbcUtil.rollback();
 				}
 			}
 
-//			String sql3 = "INSERT INTO CUSTOMMEALKITING VALUES(?, ?, ?)"; // CUSTOMMEALKITING�� �߰�
+//			String sql3 = "INSERT INTO CUSTOMMEALKITING VALUES(?, ?, ?)"; 
 //			for (CustomMealkit item : l) {
 //				int custommealkitId = item.getCustomMealkitId();
 //				List<Ingredient> customIng = getCustomIngList(item);
@@ -179,7 +173,7 @@ public class OrderDAO {
 //					param = new Object[] { custommealkitId, ing.getIngId(), ing.getIngQuantity() };
 //					jdbcUtil.setSqlAndParameters(sql3, param);
 //
-//					int result = jdbcUtil.executeUpdate(); // ó���� ���ڵ� ����
+//					int result = jdbcUtil.executeUpdate(); 
 //					if (result != 1) {
 //						jdbcUtil.rollback();
 //					}
@@ -195,7 +189,6 @@ public class OrderDAO {
 		return orderingItems;
 	}
 	
-	//�ֹ����
 	public int deleteOrder(int orderId) { //void?
 		String sql = "UPDATE mealkitorder "
 				+ "SET status = 3 "
@@ -203,20 +196,20 @@ public class OrderDAO {
 		jdbcUtil.setSqlAndParameters(sql, new Object[] {orderId});
 		
 		try {				
-			int result = jdbcUtil.executeUpdate();	// update �� ����
+			int result = jdbcUtil.executeUpdate();	
 			return result;
 		} catch (Exception ex) {
 			jdbcUtil.rollback();
 			ex.printStackTrace();
 		} finally {
 			jdbcUtil.commit();
-			jdbcUtil.close();	// resource ��ȯ
+			jdbcUtil.close();	
 		}
 		
 //		String sql1 = "UPDATE CUSTOMMEALKIT SET orderstatus = 0 WHERE custommkId = ?";
 //		jdbcUtil.setSqlAndParameters(sql1, new Object[] {cmkId});
 //		try {				
-//			int result = jdbcUtil.executeUpdate();	// update �� ����
+//			int result = jdbcUtil.executeUpdate();	
 //			return result;
 //		} catch (Exception ex) {
 //			jdbcUtil.rollback();
@@ -229,12 +222,10 @@ public class OrderDAO {
 		List<Order> orderList = new ArrayList<>();
 
 		String sql = "SELECT DISTINCT mo.orderid, mo.orderdate, mo.status, mo.totalprice FROM mealkitorder mo, orderinfo o WHERE mo.orderid=o.orderid AND customerid=? ORDER BY mo.orderid";
-		// �ֹ���ȣ, �ֹ�����, �ֹ�����, ��ŰƮ��, ����, ����, ��Į�θ�
 		jdbcUtil.setSqlAndParameters(sql, new Object[] {customerId});
 		ResultSet rs = null;
 		try {
 			rs = jdbcUtil.executeQuery();
-			// update �� ����
 			while (rs.next()) {
 				orderList.add(new Order(rs.getInt("orderid"), customerId, rs.getInt("status"),
 						rs.getInt("totalprice"), rs.getString("orderdate").split(" ")[0]));
@@ -247,11 +238,10 @@ public class OrderDAO {
 			if (rs != null) { try {
 				rs.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}}
 			jdbcUtil.commit();
-			jdbcUtil.close();	// resource ��ȯ
+			jdbcUtil.close();	
 		}
 		
 		return null;
