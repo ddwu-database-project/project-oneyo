b632f08a3900f1a08b8ddf9ae6e9872306d66dac
HyeJun <solar3070@naver.com>
2021-12-02 02:26:24 +0900

    주문 취소시 공유 취소

--- project-oneyo/src/main/java/controller/order/DeleteOrderController.java
+++ project-oneyo/src/main/java/controller/order/DeleteOrderController.java
@@ -5,16 +5,27 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import controller.Controller;
+import controller.customer.CustomerSessionUtils;
+import model.dao.CustomMkDAO;
+import model.dao.CustomerDAO;
 import model.dao.OrderDAO;
+import model.dto.Customer;
 
 public class DeleteOrderController implements Controller {
 	public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception{
 //		HttpSession session = request.getSession();
 //		int orderId = (int) session.getAttribute("orderId");
 //		int cmkId = (int) session.getAttribute("cmkId");
-//		
+		
     	OrderDAO orderDAO = new OrderDAO();
     	orderDAO.deleteOrder(Integer.parseInt(request.getParameter("orderid")));
+    	
+		int customMkId = Integer.parseInt(request.getParameter("custommkId"));
+		CustomMkDAO customMkDAO = new CustomMkDAO();
+		
+		if (customMkDAO.updateShare(customMkId, 0) == 0) {
+			System.out.println("���� ��� ����");
+		}
     	
     	//redirection
 		return "redirect:/order/list";
