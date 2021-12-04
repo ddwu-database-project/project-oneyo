17ec25cf7e77741d6bc98b7e81995571946dd6d6
gynhyunkim <gyeonghyun.kimm@gmail.com>
2021-12-05 00:10:04 +0900

    update: redirect uri 수정, details.jsp 수정

--- project-oneyo/src/main/java/controller/review/AddReviewController.java
+++ project-oneyo/src/main/java/controller/review/AddReviewController.java
@@ -1,20 +1,11 @@
 package controller.review;
 
-import java.io.Console;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import controller.Controller;
-import controller.customer.CustomerSessionUtils;
-import model.dao.CustomerDAO;
-import model.dao.MealkitDAO;
 import model.dao.ReviewDAO;
-import model.dto.Customer;
-import model.dto.Ingredient;
-import model.dto.Mealkit;
 import model.dto.Review;
 
 public class AddReviewController implements Controller {
@@ -44,26 +35,7 @@ public class AddReviewController implements Controller {
 		
 		reviewDAO.newReview(tmp);
 		
-		MealkitDAO mealkitDAO = new MealkitDAO();
-		
-		Mealkit mealkit =mealkitDAO.findMealkit(mkId);
-		List<Ingredient> mealkitIng = mealkitDAO.findMealkitIng(mkId);
-
-		mealkit.setIngredients(mealkitIng);
-		
-		request.setAttribute("mealkit", mealkit);	
-
-		CustomerDAO customerDAO = new CustomerDAO();
-		CustomerSessionUtils u = new CustomerSessionUtils();
-		HttpSession session = request.getSession();	
-		@SuppressWarnings("static-access")
-		String email = u.getLoginCustomerId(session);
-		Customer c = customerDAO.findCustomer(email);
-		
-		request.setAttribute("loginCustomerId", c.getCustomerId());
-		
-		
-		return "/mealkit/details.jsp";   
+		return "redirect:/mealkit/detail?mkId=" + mkId;   
 
 	}
 
