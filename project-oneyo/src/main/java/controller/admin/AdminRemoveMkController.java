707f61b45c31cd53e1ee1c63a490bd8a2a48a676
Hyo Gyeong <cdkrcd8@gmail.com>
2021-12-17 21:19:27 +0900

    이미지 삭제시 파일도 삭제됨

--- project-oneyo/src/main/java/controller/admin/AdminRemoveMkController.java
+++ project-oneyo/src/main/java/controller/admin/AdminRemoveMkController.java
@@ -1,5 +1,8 @@
 package controller.admin;
 
+import java.io.File;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,7 +16,21 @@ public class AdminRemoveMkController implements Controller {
 		// TODO Auto-generated method stub
 		MealkitDAO mkDAO = new MealkitDAO();
 		
-		mkDAO.remove(Integer.parseInt(request.getParameter("id")));
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		//delete old file in img folder
+		ServletContext context = request.getServletContext();
+		String path = context.getRealPath("/");
+		int idx = path.indexOf(".metadata");
+		path = path.substring(0, idx);
+		path += "project-oneyo/project-oneyo/src/main/webapp/assets/img";
+		File dir = new File(path);
+        String oldFile = mkDAO.findMealkit(id).getFilename();
+        String oldPath = dir +  "/" + oldFile;
+        File delFile = new File(oldPath);
+        if (delFile.exists()) delFile.delete();
+		
+		mkDAO.remove(id);
 		return "redirect:/admin/mealkit/list";
 	}
 
